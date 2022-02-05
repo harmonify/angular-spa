ef98d8368797aa17c32500fc57f9ac05acb8bfa0
Wendy Surya Wijaya <84518752+harmonify@users.noreply.github.com>
2022-02-05 23:18:38 +0700

    edit breadcrumb description

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -42,7 +42,7 @@ const routes: Routes = [
         path: ':id/edit',
         component: DivisionEditComponent,
         data: {
-          breadcrumb: 'Edit Division',
+          breadcrumb: 'Edit',
         },
       },
     ],
